d52eeaa4da498fe83e5abe9d4346bf6971274732
Adele4242 <adel.belekova@bk.ru>
2022-07-29 22:24:19 +0600

    initial

---
+++ main.js
@@ -0,0 +1,129 @@
+
+//console.log(1224)
+
+//alert(13224141354)
+
+//promt('How old are you?')
+
+//confirm('Are you sure?')
+
+
+
+//let const ////var
+
+
+
+//let number = 100
+    //number = 150
+
+//const name = 'Adele'
+      //name = 'Aruuke'
+
+//console.log(number)
+//console.log(name)
+
+
+
+//let boy
+
+//console.log(boy)
+
+
+
+//let day24 = 'Adele'
+//let firstName = 'Adele'
+//let $na_me = 'Adele'
+
+//let name = 'Adele'
+//alert(name)
+
+//let name = promt('What is your name?')
+//
+//console.log(name)
+
+
+//let leave = confirm('Are you sure?')
+//console.log(leave)
+
+
+//types
+//number, string, boolean, undefined, null, object, ----symbol,bigInt
+
+//let number = 400
+//let string = 'Adele', "Adele"
+//let boolean = false
+//let undefined = undefined
+//let nul = null
+//primitive
+
+
+//let obj = {
+//name: 'Adele',
+//age: 27,
+//stack: 'Front-end'
+//}
+
+
+//let name = null;
+//console.log(typeof name)
+
+
+
+
+//let a = 5
+//let b =10
+//
+//console.log(a + b)
+//console.log(a - b)
+//console.log(a * b)
+//console.log(a / b)
+//console.log(a ** b)  //stepen
+
+//console.log(21 % 5)  //ostatok
+//21 - 5 =16 - 5 =11-5 =6-5=1
+
+
+//let a = 0
+//let b = 3
+//console.log(typeof (b / a)) //type: number
+
+
+
+//let a = '2'
+//let b = 3
+//console.log(a + b) //concatination/sliyanie
+//console.log(a - b) //if there are no letters, function will work
+//console.log(a * b)  //if there are, then will get NuN
+//console.log(a / b)
+//
+
+
+//let a = '5'
+//console.log(-a) //will get -5
+//
+//let a = 'Adele'
+//console.log(+a) //will get Nan
+
+
+let a = 5
+let b = 3
+
+console.log(a >= b) //true
+console.log(a <= b) //false
+//console.log(a > b) //true
+//console.log(a < b) //false
+//console.log(a = b)  //not strict
+//console.log(a == b)  //strict type of sravnivaetsya
+//console.log(a === b)  //strict znachenie
+
+
+
+
+
+
+
+
+
+
+
+
